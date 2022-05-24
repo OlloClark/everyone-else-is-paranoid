453b8682e8d7cc062fbb2988aec2bd79abe2e755
OlloClark <ollo42@hotmail.co.uk>
2022-05-24 16:54:38 -0700

    adding async function SuspectFeedPage

--- src/pages/SuspectFeedPage/SuspectFeedPage.jsx
+++ src/pages/SuspectFeedPage/SuspectFeedPage.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TopNav from "../../components/TopNav/TopNav";
 import AddSuspect from "../../components/NewSuspectForm/NewSuspectForm";
-import SuspectFeed from '../../components/SuspectFeed/SuspectFeed'; 
+import SuspectFeed from '../../components/SuspectFeed/SuspectFeed';
+import * as suspectsAPI from "../../utils/suspectAPI";
 
 
 
 function SuspectFeedPage({user, handleLogout}) {
+	const [suspects, setSuspects] = useState([])
+
+	async function handleAddSuspect(suspect) {
+		const data = await suspectsAPI.create(suspect)
+		console.log(data, "<- this is the data")
+	}
 
 
 	return(
 	<>
 	<TopNav user = {user} handleLogout={handleLogout} />
 	<h1>Logged in. Suspect Feed here</h1>
-	<AddSuspect />
+	<AddSuspect handleAddSuspect={handleAddSuspect}/>
 	<SuspectFeed />
 
 	
