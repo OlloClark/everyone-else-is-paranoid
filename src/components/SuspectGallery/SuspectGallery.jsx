38ad1fe4a6a63a549641c20846c32b4cf0d5d9ba
OlloClark <ollo42@hotmail.co.uk>
2022-05-24 19:37:28 -0700

    adding basics of SuspectGallery

--- src/components/SuspectGallery/SuspectGallery.jsx
+++ src/components/SuspectGallery/SuspectGallery.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Card, Dimmer, Segment, Image  } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
 import SuspectCard from '../SuspectCard/SuspectCard';
 
-function SuspectGallery(props) {
+function SuspectGallery({suspects, numPhotosCol, user}) {
 	return(
-		<></>
+		<Card.Group itemsPerRow={numPhotosCol} stackable>
+        {suspects.map((suspect) => {
+          return (
+            <SuspectCard
+              suspect={suspect}
+              key={suspect._id}
+              user={user}
+            />
+          );
+        })}
+      </Card.Group>
 	)
 }
 
