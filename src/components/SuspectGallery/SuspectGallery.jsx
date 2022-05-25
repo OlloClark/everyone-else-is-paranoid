c40000c1840cc873bf9215ab73716a2807d2b0ed
OlloClark <ollo42@hotmail.co.uk>
2022-05-24 19:33:57 -0700

    adding empty suspectGallery

---
+++ src/components/SuspectGallery/SuspectGallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, Dimmer, Segment, Image  } from 'semantic-ui-react'
+import SuspectCard from '../SuspectCard/SuspectCard';
+
+function SuspectGallery(props) {
+	return(
+		<></>
+	)
+}
+
+export default SuspectGallery
