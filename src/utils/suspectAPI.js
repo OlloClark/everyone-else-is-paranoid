c54a1f5dc648955ad7e11407c4c4f3a933ad9459
OlloClark <ollo42@hotmail.co.uk>
2022-05-25 17:40:26 -0700

    suspectAPI adjustment

--- src/utils/suspectAPI.js
+++ src/utils/suspectAPI.js
@@ -10,13 +10,20 @@ export function create(suspect) {
         'Authorization': 'Bearer ' + tokenService.getToken()
       }
     
-    }).then(res => res.json());
+    }).then(res => {
+      if(res.ok) return res.json();
+      throw new Error('Bad Credentials! CHECK THE SERVER TERMINAL!')
+    })
   }
 
   export function getAll() {
-	  return fetch(BASE_URL, {
-		  headers: {
-			  Authorization: "Bearer " + tokenService.getToken(),
-		  },
-	  }).then((res) => res.json());
+    return fetch(BASE_URL, {
+      headers: {
+        'Authorization': 'Bearer ' + tokenService.getToken()
+      }
+    })
+    .then(res => {
+      if(res.ok) return res.json();
+      throw new Error('Bad Credentials! CHECK THE SERVER TERMINAL!')
+    })
   }
