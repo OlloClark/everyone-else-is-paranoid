683bf0620bec40f0dee409a9d2c575f7feb0c651
OlloClark <ollo42@hotmail.co.uk>
2022-05-26 20:16:44 -0700

    khsagfkagfkaer

--- routes/api/snoops.js
+++ routes/api/snoops.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const snoopsCtrl = require('../../controllers/snoops')
 
-router.post('/posts/:id/snoops', snoopsCtrl.create)
+router.post('/suspects/:id/snoops', snoopsCtrl.create)
 router.delete('/snoops/:id', snoopsCtrl.deleteSnoop)
 
 module.exports = router;
